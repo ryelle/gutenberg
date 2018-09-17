f6edc43d5193dd1571391e20cc05ccfc6771afc9
Kelly Dwan <ryelle@users.noreply.github.com>
2018-09-17 14:33:57 -0400

    Export Token component from FormTokenField

--- packages/components/src/index.js
+++ packages/components/src/index.js
@@ -53,6 +53,7 @@ export { default as TabPanel } from './tab-panel';
 export { default as TextControl } from './text-control';
 export { default as TextareaControl } from './textarea-control';
 export { default as ToggleControl } from './toggle-control';
+export { default as Token } from './form-token-field/token';
 export { default as Toolbar } from './toolbar';
 export { default as Tooltip } from './tooltip';
 export { default as TreeSelect } from './tree-select';
